cfc113b67e4dc657a563cba5b2648a834f08df66
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2014-01-06 13:06:50 -0200

    Melhorias
    Melhorias sugeridas por Fernando Concatto

--- Java/Exercicios/Tabuada - JOptionPane/src/Main.java
+++ Java/Exercicios/Tabuada - JOptionPane/src/Main.java
@@ -4,18 +4,30 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		String tabu = JOptionPane.showInputDialog("Tabuada que deseja acessar:");
-		int iTabu;
+		String opcaoTabuada = JOptionPane.showInputDialog("Tabuada que deseja acessar:");
+		int indiceTabuada;
 		int[] res;
-		
+
 		res = new int[10];
-		
-		iTabu = Integer.parseInt(tabu);
-		
-		for(int i=1;i <= 10;i++){
-			res[i-1] = i * iTabu;
+
+		indiceTabuada = Integer.parseInt(opcaoTabuada);
+
+		for(int i = 0;i < res.length; i++){
+			res[i] = (i + 1) * indiceTabuada;
 		}
-		//Exibição dos resultados muito longa. Descobrir forma reduzida
-		JOptionPane.showMessageDialog(null, tabu + "x1=" + + res[0] + "\n" + tabu + "x2=" + res[1] + "\n" + tabu + "x3=" + res[2] + "\n" + tabu + "x4=" + res[3] + "\n" + tabu + "x5=" + res[4] + "\n" + tabu + "x6=" + res[5] + "\n" + tabu + "x7=" + res[6] + "\n" + tabu + "x8=" + res[7] + "\n" + tabu + "x9=" + res[8] + "\n" + tabu + "x10=" + res[9], "Tabuada", JOptionPane.ERROR_MESSAGE);
+		//JOptionPane não da opções para redução. Redução no código só poderá ser feita usando JFrame com JTextField, utilizando laços.
+		JOptionPane.showMessageDialog(null,
+				opcaoTabuada + "x1=" + res[0]+ "\n"
+						+ opcaoTabuada + "x2=" + res[1] + "\n"
+						+ opcaoTabuada + "x3=" + res[2] + "\n"
+						+ opcaoTabuada + "x4=" + res[3] + "\n"
+						+ opcaoTabuada + "x5=" + res[4] + "\n"
+						+ opcaoTabuada + "x6=" + res[5] + "\n"
+						+ opcaoTabuada + "x7=" + res[6] + "\n"
+						+ opcaoTabuada + "x8=" + res[7] + "\n"
+						+ opcaoTabuada + "x9=" + res[8] + "\n"
+						+ opcaoTabuada + "x10=" + res[9],
+						"Tabuada", JOptionPane.ERROR_MESSAGE
+				);
 	}
 }
